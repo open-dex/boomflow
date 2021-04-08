73388c0eafc311c5629837b24f40c8681045d543
boqiu <82121246@qq.com>
2021-04-09 00:27:13 +0800

    Fix build error for examples

--- src/examples/boomflow/SettlementDemo.java
+++ src/examples/boomflow/SettlementDemo.java
@@ -5,19 +5,20 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import org.web3j.protocol.core.Response.Error;
+
+import boomflow.common.Address;
 import boomflow.eip712.core.Domain;
+import boomflow.worker.CfxSettlementWorker;
 import boomflow.worker.SettlementHandler;
 import boomflow.worker.SettlementWorker;
-import boomflow.worker.TransactionRelayer;
 import boomflow.worker.settle.PositionSettlement;
 import boomflow.worker.settle.Settleable;
 import boomflow.worker.settle.SettlementStatus;
 import conflux.web3j.Account;
 import conflux.web3j.Cfx;
 import conflux.web3j.CfxUnit;
-import conflux.web3j.types.CfxAddress;
 import conflux.web3j.types.RawTransaction;
-import conflux.web3j.types.SendTransactionResult;
 
 public class SettlementDemo {
 
@@ -28,7 +29,6 @@ public class SettlementDemo {
 //		TransactionRelayer relayer = new TransactionRelayer(executor);
 //		relayer.addServer("http://test1.confluxrpc.org");
 //		relayer.addServer("http://test2.confluxrpc.org");
-		TransactionRelayer relayer = null;
 		
 		Cfx cfx = MonitorEventLogDemo.connectTestnet();
 		Account admin = Account.create(cfx, "0xbe2fbe75ed7226147fc382620c706be2bc999c00268d58ea638b1627cdee6eab");
@@ -42,9 +42,12 @@ public class SettlementDemo {
 		RawTransaction.setDefaultChainId(cfx.getStatus().sendAndGet().getChainId());
 		Domain.defaultName = "MiragexToken";
 		Domain.defaultChainId = RawTransaction.getDefaultChainId().longValueExact();
-		Domain.boomflowAddress = new CfxAddress("cfxtest:acdf0hmrmzfdz29whnpvm81mk7atcfdkbex3tavgfk");
+		Domain.boomflowAddress = Address.createCfxAddress("cfxtest:acdf0hmrmzfdz29whnpvm81mk7atcfdkbex3tavgfk");
 		
-		SettlementWorker worker = new SettlementWorker(executor, admin, new ConsoleSettlementHandler(), relayer);
+		SettlementWorker worker = new CfxSettlementWorker(executor, admin, new ConsoleSettlementHandler());
+		
+		// set tx relayer to faster tx propagation
+//		worker.setTxRelayer(relayer);
 		
 		executor.scheduleWithFixedDelay(new Runnable() {
 			
@@ -73,9 +76,9 @@ public class SettlementDemo {
 			@Override
 			protected TypedPosition toTypedData() {
 				return new TypedPosition(1,
-						new CfxAddress("cfxtest:aar61c2b0cvt4gr2m4ytn6xe7a55s261me411kwwkc"),
-						new CfxAddress("cfxtest:aan02vpwvz8crpa1n10j17ufceefptdc2y9cx0zd94"),
-						new CfxAddress("cfxtest:achf549sa9mxsge2dtzme1mvftm5vv69h2u13t9hew"),
+						Address.createCfxAddress("cfxtest:aar61c2b0cvt4gr2m4ytn6xe7a55s261me411kwwkc"),
+						Address.createCfxAddress("cfxtest:aan02vpwvz8crpa1n10j17ufceefptdc2y9cx0zd94"),
+						Address.createCfxAddress("cfxtest:achf549sa9mxsge2dtzme1mvftm5vv69h2u13t9hew"),
 						1, 1, 1, 1, 1, 1, 1, "CFX/USDT");
 			}
 			
@@ -122,8 +125,8 @@ class ConsoleSettlementHandler implements SettlementHandler {
 	}
 
 	@Override
-	public void onUnexpectedTransactionError(Settleable data, SendTransactionResult result) {
-		System.out.println("[HANDLER] onUnexpectedTransactionError, result = " + result);
+	public void onUnexpectedTransactionError(Settleable data, Error error) {
+		System.out.println("[HANDLER] onUnexpectedTransactionError, erorr = " + error);
 	}
 
 	@Override
